a9dcfee9ad6f07bfbfba5f8e29ba99ffc37251ab
chenli <767471286@qq.com>
2026-04-29 13:54:49 +0800

    add debug log

--- apisix/plugins/proxy-buffering.lua
+++ apisix/plugins/proxy-buffering.lua
@@ -46,6 +46,8 @@ end
 
 -- we want to control proxy behavior before auth, so put the code under rewrite method
 function _M.rewrite(conf, ctx)
+    core.log.warn("proxy-buffering rewrite called, enable=", conf.enable, ", ok=", ok)
+
     if not ok then
         core.log.error("need to build APISIX-Runtime to support proxy buffering")
         return 501
@@ -54,6 +56,7 @@ function _M.rewrite(conf, ctx)
     local enable = conf.enable
     if enable ~= nil then
         local ok, err = apisix_ngx_client.set_proxy_buffering(enable)
+        core.log.warn("set_proxy_buffering(", enable, ") ret=", ok, " err=", err)
         if not ok then
             core.log.error("failed to set proxy_buffering: ", err)
             return 503
